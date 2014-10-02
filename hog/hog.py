fc6823c4b70340daa2e52659931cbaf01f82d6d6
Evan Savage <savage.evan@gmail.com>
2014-10-02 09:53:40 -0700

    Hog: bacon_strategy()

--- hog/hog.py
+++ hog/hog.py
@@ -209,10 +209,10 @@ def run_experiments():
         four_sided_max = max_scoring_num_rolls(four_sided)
         print('Max scoring num rolls for four-sided dice:', four_sided_max)
 
-    if False: # Change to True to test always_roll(8)
+    if True: # Change to True to test always_roll(8)
         print('always_roll(8) win rate:', average_win_rate(always_roll(8)))
 
-    if False: # Change to True to test bacon_strategy
+    if True: # Change to True to test bacon_strategy
         print('bacon_strategy win rate:', average_win_rate(bacon_strategy))
 
     if False: # Change to True to test swap_strategy
@@ -236,8 +236,10 @@ def bacon_strategy(score, opponent_score):
     >>> bacon_strategy(50, 70)
     0
     """
-    "*** YOUR CODE HERE ***"
-    return 5 # Replace this statement
+    bacon = max(opponent_score % 10, opponent_score // 10) + 1
+    if bacon >= BACON_MARGIN:
+      return 0
+    return BASELINE_NUM_ROLLS
 
 def swap_strategy(score, opponent_score):
     """This strategy rolls 0 dice when it would result in a beneficial swap and
