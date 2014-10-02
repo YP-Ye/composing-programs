15276e8342c77b0c86cbc9a8645be7f11625af66
Evan Savage <savage.evan@gmail.com>
2014-10-02 09:16:51 -0700

    Hog Q2: take_turn()

--- hog/hog.py
+++ hog/hog.py
@@ -44,7 +44,9 @@ def take_turn(num_rolls, opponent_score, dice=six_sided):
     assert num_rolls >= 0, 'Cannot roll a negative number of dice.'
     assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
     assert opponent_score < 100, 'The game should be over.'
-    "*** YOUR CODE HERE ***"
+    if num_rolls == 0:
+        return max(opponent_score % 10, opponent_score // 10) + 1
+    return roll_dice(num_rolls, dice)
 
 # Playing a game
 
