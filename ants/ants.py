8558f56f65e3e7fba87fe7a64ab64f13dd76d187
Evan Savage <savage.evan@gmail.com>
2014-10-20 20:34:53 -0700

    Q3: Place exit.entrance

--- ants/ants.py
+++ ants/ants.py
@@ -29,7 +29,8 @@ class Place:
         self.ant = None       # An Ant
         self.entrance = None  # A Place
         # Phase 1: Add an entrance to the exit
-        "*** YOUR CODE HERE ***"
+        if self.exit is not None:
+            self.exit.entrance = self
 
     def add_insect(self, insect):
         """Add an Insect to this Place.
